86beee05b236f3f5c8732045ce8a1ef9a5507d0d
Jenil Shah <131328441+Jenil9761@users.noreply.github.com>
2024-07-04 15:22:49 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 pwd
+mkdir todo
+# pwd
+# sudo rsync -av --exclude='todo' * /home/ubuntu/todo
+# pwd
+# sudo cp -r /home/ubuntu/todo /var/www/todo/
+# pwd
+# cd /var/www/todo/ || { echo "Directory /var/www/todo/ not found. Exiting."; exit 1; }
+# pwd
+# if ! command -v npm &> /dev/null; then
+#     echo "npm not found. Please ensure npm is installed and in the PATH."
+#     exit 1
+# fi
+# npm install
+# if ! command -v pm2 &> /dev/null; then
+#     echo "pm2 not found. Installing pm2 globally."
+#     npm install pm2 -g
+# fi
+# pm2 start

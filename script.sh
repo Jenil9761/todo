693a68c45f707c1a92e67506f3da016ed6a7645e
Jenil Shah <131328441+Jenil9761@users.noreply.github.com>
2024-07-04 16:06:54 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,10 +5,10 @@ timeout 30s mkdir todo
 pwd
 timeout 60s sudo rsync -av --exclude='todo' * /home/ubuntu/todo
 pwd
-timeout 60s sudo cp -r /home/ubuntu/todo /var/www/todo/
+timeout 60s sudo cp -r /home/ubuntu/todo /var/www/
 pwd
+rm -rf /home/ubuntu/*
 cd /var/www/todo/
 pwd
 timeout 100s sudo npm install
-timeout 100s sudo npm install pm2 -g
 sudo pm2 start

254d01c78400abcf7449c4874d0811bc25cc2584
Jenil Shah <131328441+Jenil9761@users.noreply.github.com>
2024-07-04 19:15:47 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 pwd
-timeout 30s mkdir todo
+timeout 3s mkdir todo
 pwd
-timeout 60s sudo rsync -av --exclude='todo' * /home/ubuntu/todo
+timeout 5s sudo rsync -av --exclude='todo' * /home/ubuntu/todo
 pwd
-timeout 60s sudo mv -r /home/ubuntu/todo /var/www/
+timeout 5s sudo mv -r /home/ubuntu/todo /var/www/
 pwd
 timeout 10s rm -rf /home/ubuntu/*
 timeout 10s cd /var/www/todo/
 pwd
-timeout 100s sudo npm install
-timeout 100s sudo pm2 start app.js
-timeout 100s pm2 save
+timeout 10s sudo npm install
+timeout 10s sudo pm2 start app.js
+timeout 10s pm2 save
 
 # #!/bin/bash
 

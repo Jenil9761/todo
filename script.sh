d4f271b91249f3f16d48db0d19dce36377b32fe2
Jenil Shah <131328441+Jenil9761@users.noreply.github.com>
2024-07-04 19:11:29 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,19 +1,3 @@
-# #!/bin/bash
-
-# pwd
-# timeout 30s mkdir todo
-# pwd
-# timeout 60s sudo rsync -av --exclude='todo' * /home/ubuntu/todo
-# pwd
-# timeout 60s sudo mv -r /home/ubuntu/todo /var/www/
-# pwd
-# # timeout 10s rm -rf /home/ubuntu/*
-# timeout 10s cd /var/www/todo/
-# pwd
-# timeout 100s sudo npm install
-# timeout 100s sudo pm2 start app.js
-# timeout 100s pm2 save
-
 #!/bin/bash
 
 pwd
@@ -21,11 +5,27 @@ timeout 30s mkdir todo
 pwd
 timeout 60s sudo rsync -av --exclude='todo' * /home/ubuntu/todo
 pwd
-timeout 60s sudo cp -r /home/ubuntu/todo /var/www/
+timeout 60s sudo mv -r /home/ubuntu/todo /var/www/
 pwd
-# timeout 10s rm -rf /home/ubuntu/*
+timeout 10s rm -rf /home/ubuntu/*
 timeout 10s cd /var/www/todo/
 pwd
 timeout 100s sudo npm install
 timeout 100s sudo pm2 start app.js
 timeout 100s pm2 save
+
+# #!/bin/bash
+
+# pwd
+# timeout 30s mkdir todo
+# pwd
+# timeout 60s sudo rsync -av --exclude='todo' * /home/ubuntu/todo
+# pwd
+# timeout 60s sudo cp -r /home/ubuntu/todo /var/www/
+# pwd
+# # timeout 10s rm -rf /home/ubuntu/*
+# timeout 10s cd /var/www/todo/
+# pwd
+# timeout 100s sudo npm install
+# timeout 100s sudo pm2 start app.js
+# timeout 100s pm2 save
